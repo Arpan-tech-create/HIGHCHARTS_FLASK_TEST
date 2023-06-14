a3b862a5ae4a4e5f49eccc92a71874c01036c495
Arpan-tech-create <bhoiarpan123@gmail.com>
2023-06-14 23:29:32 +0530

    'hayyyyarrr'

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-from flask import Flask, render_template, jsonify
+from flask import Flask, render_template, jsonify,request
+from pytz import timezone
 import pandas as pd
 
 app = Flask(__name__)
@@ -13,7 +14,7 @@ def index():
 
     # Extract the date and hour from the timestamp
     data['Date'] = data['TIMESTAMP'].dt.date
-    data['Hour'] = data['TIMESTAMP'].dt.strftime('%H:%M:%S')
+    data['Hour'] = data['TIMESTAMP'].dt.strftime('%H')
 
     # Count the occurrences of each date
     date_counts = data['Date'].value_counts().sort_index()
@@ -34,8 +35,28 @@ def index():
             hourly_data_dict[date] = []
         hourly_data_dict[date].append([hour, count])
 
-    # Pass the original data and hourly data to the template
-    return render_template('chart.html', original_data=original_data, hourly_data=hourly_data_dict)
+    # Filter the data based on the date range provided in the query parameters
+    from_date = request.args.get('from-date')
+    to_date = request.args.get('to-date')
+
+    if from_date and to_date:
+        from_date = pd.to_datetime(from_date).tz_localize(timezone('UTC'))
+        to_date = pd.to_datetime(to_date).tz_localize(timezone('UTC'))
+        filtered_data = data[(data['TIMESTAMP'] >= from_date) & (data['TIMESTAMP'] <= to_date)]
+    else:
+        filtered_data = data
+
+    # Filter the data for RESPONSE 403
+    filtered_data = filtered_data[filtered_data['RESPONSE'] == 403]
+
+    # Count the occurrences of each message
+    message_counts = filtered_data['MESSAGE'].value_counts()
+
+    # Prepare the data for the pie chart
+    pie_data = [{'name': message, 'y': count} for message, count in message_counts.items()]
+
+    # Pass the original data, hourly data, and pie data to the template
+    return render_template('chart.html', original_data=original_data, hourly_data=hourly_data_dict, pie_data=pie_data, from_date=from_date, to_date=to_date)
 
 if __name__ == '__main__':
-    app.run()
+    app.run(debug=True, port=5002)
